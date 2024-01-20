614ed3d4fd924511107efc202c9ce52f1e705b6c
Naman-9 <namancp7@gmail.com>
2024-01-20 11:47:18 +0530

    Primary Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import {BrowserRouter ,Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <h1 className='text-orange-300'>Working....</h1>
-  )
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+
+    )
 }
 
 export default App
