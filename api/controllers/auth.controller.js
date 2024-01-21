6a67af4cb5b5bcfd40ff2fbfcc1d37df8cb09e11
Naman-9 <namancp7@gmail.com>
2024-01-21 16:59:02 +0530

    sign In APi

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
         next(errorHandler(400, "All feilds are required."));
- }
+    }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -17,7 +17,7 @@ export const signUp = async (req, res, next) => {
         email,
         password: hashedPassword
     });
- 
+
     try {
         await newUser.save();
         res.json("SignUp Successful.")
@@ -25,5 +25,46 @@ export const signUp = async (req, res, next) => {
         next(error);
     }
 
-  
+
+};
+
+export const signIn = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        return next(errorHandler(400, "All feilds are required."));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+
+        if (!validUser) {
+            return next(errorHandler(404, "Invalid User Credentials."));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) {
+            return next(errorHandler(404, "Invalid User Credentials."));
+        }
+
+        const token = jwt.sign(
+            { id: validUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        const {password: pass, ...rest} = validUser._doc;
+
+        res.status(200).cookie(
+            'access_token', token,{
+                httpOnly: true
+            }).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+
+
+
 };
