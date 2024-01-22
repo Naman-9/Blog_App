b71b8fe6bf6ba94bfaa567f9c5d03265f0e893ee
Naman-9 <namancp7@gmail.com>
2024-01-22 17:09:06 +0530

    dashboard sidebar UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl) setTab(tabFromUrl);
+  }, [location]);
   return (
-    <div>Dashboard</div>
-  )
+    <div className='flex min-h-screen flex-col md:flex-row'>
+      {/* Sidebar */}
+      <div className="">
+        <DashSidebar />
+      </div>
+      {/* Profile Comp */}
+      {tab === 'profile' && (
+        <div className="">
+          <DashProfile />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
