028bcbef887e3f7924aa64bb425749ba60bb945c
Naman-9 <namancp7@gmail.com>
2024-01-22 16:30:51 +0530

    PrivateRoute setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,31 @@
-import './App.css'
-import {BrowserRouter ,Routes, Route} from 'react-router-dom';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignOut from './pages/SignOut';
 import Dashboard from './pages/Dashboard';
-import { Header, FooterComp  } from './components';
+import { Header, FooterComp } from './components';
 import SignUp from './pages/SignUp';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-out' element={<SignOut />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
-      <FooterComp  />
+      <FooterComp />
     </BrowserRouter>
-
-    )
+  );
 }
 
-export default App
- 
+export default App;
