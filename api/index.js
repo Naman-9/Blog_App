b9f06648bf22d3bd8c959101b4279c8a46de803c
Naman-9 <namancp7@gmail.com>
2024-01-24 17:33:04 +0530

    create Post Api Route

--- api/index.js
+++ api/index.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import mongoose, { Mongoose } from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
+import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 dotenv.config();   
 
 mongoose.connect(process.env.MONGO)
 .then(() => console.log("DataBase Connected...."))
-.catch((err) => console.log("Database not connected--", err));
+.catch((err) => console.log("Database not connected--", err)); 
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 
 app.use((err, req, res, next) => {
