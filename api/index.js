9043ce1c2629729a96c01b743361f236348c7a03
Naman-9 <namancp7@gmail.com>
2024-01-29 07:53:48 +0530

    index changes

--- api/index.js
+++ api/index.js
@@ -8,43 +8,45 @@ import commentRoutes from './routes/comment.routes.js'
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
-dotenv.config();   
+dotenv.config();
 
-mongoose.connect(process.env.MONGO) 
-.then(() => console.log("DataBase Connected...."))
-.catch((err) => console.log("Database not connected--", err)); 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  });
-  
-
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal server Error";
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
-})
-
-
-
-app.listen(process.env.PORT,() => {
-    console.log("running.....");
-})
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
